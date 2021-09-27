3302888eca16b8b9521b6ae8458773d63551c403
dssalazaru <84487948+dssalazaru@users.noreply.github.com>
2021-09-26 21:50:31 -0500

    Avances sobre sprint 2 y sprint 3 en inventory

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 function toggleSearch() {
     document.getElementById("in-buscar").classList.toggle('hidden');
+    document.getElementById("btn-search").classList.toggle('search-close');
     document.getElementById("in-buscar").focus();
 };
