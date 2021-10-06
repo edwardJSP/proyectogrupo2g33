3a5f55b25e4de367dafe9499d98b92e1ae727817
dssalazaru <dssalazaru@outlook.com>
2021-10-06 17:46:24 -0500

    mejoras en diseño responsivo de inventario

--- js/main.js
+++ js/main.js
@@ -3,3 +3,7 @@ function toggleSearch() {
     document.getElementById("btn-search").classList.toggle('search-close');
     document.getElementById("in-buscar").focus();
 };
+function toggleMenuUser() {
+    document.getElementById("menu-user").classList.toggle('hidden');
+    document.getElementById("btn-user").classList.toggle('menu-user-toggle');
+};
