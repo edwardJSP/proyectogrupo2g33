a4de5974f91fcc3db496e4e48863d588e8043ec3
dssalazaru <dssalazaru@gmail.com>
2021-10-07 01:05:00 -0500

    Version final html y css inventario

--- js/main.js
+++ js/main.js
@@ -5,5 +5,22 @@ function toggleSearch() {
 };
 function toggleMenuUser() {
     document.getElementById("menu-user").classList.toggle('hidden');
-    document.getElementById("btn-user").classList.toggle('menu-user-toggle');
+    document.getElementById("user-card").classList.toggle('menu-user-toggle');
 };
+
+function togglePanel() {
+    document.getElementById("sm1").classList.toggle('hidden');
+    document.getElementById("sm2").classList.toggle('hidden');
+    document.getElementById("sm3").classList.toggle('hidden');
+    document.getElementById("sm4").classList.toggle('hidden');
+    document.getElementById("sm5").classList.toggle('hidden');
+    document.getElementById("sm6").classList.toggle('hidden');
+    document.getElementById("sm").classList.toggle('sidebar-toggle');
+};
+
+function itemInfo() {
+    document.getElementById("item-show").classList.toggle('hidden');
+};
+
+
+    
