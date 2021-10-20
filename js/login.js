13ab82b953281a560c5c7abe5664649669fc2e39
edward julisan sandoval <edwardjuliansandoval@gmail.com>
2021-10-20 08:48:37 -0500

    corrige login

--- js/login.js
+++ js/login.js
@@ -1,15 +1,27 @@
 function ir (){
 
-    var c=2468;
-    var u="NAME";
-      if (document.forms.password.value==c && document.forms.login.value==u){
+  var c="2468";
+  var u="NAME";
+  
+  var correo = document.getElementById("inputmail").value
+  
+  var nill="";
+  var contra = document.getElementById("inputpassword").value
+  
+      if (contra==c & correo==u){
       alert("Bienvenido a G2 Inventory");
-    window.location="equipos.html";}
-    
-    else{ 
-        alert("Porfavor ingrese, nombre y usuario correcto.");
+      window.location="inventory.html";}
+        else if (contra==nill){
+        alert("No ingreso contraseña intentelo de nuevo.");}
+        else if (correo==nill){
+          alert("No ingreso un correo intentelo de nuevo.");}
+          else if (contra==nill & contra==nill){
+            alert("No ingreso contraseña ni correo intentelo de nuevo.");}
+           else{ 
+           alert("Porfavor ingrese, nombre y usuario correcto.");
+        
+      }
       
-    }
-    
-    }
-    
+      
+      
+      }
